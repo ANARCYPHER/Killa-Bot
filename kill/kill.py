f6eb2aded67a39116f17ae91a14f50baa7634c33
AllSafeCybercurity <skull32273227@gmail.com>
2022-02-17 10:20:35 -0800

    Killa Bot

---
+++ kill/kill.py
@@ -0,0 +1,16 @@
+###############################
+#       Killer Bot IA         #
+###############################
+
+name = input("What's is your F*ck Name! B!tch??\n")
+
+if name == "Killa":
+   psycho_status = input("U are F*ck Sick?\n") 
+   if psycho_status == "Yes":  
+       print("Get Out!!")
+       exit()
+   else:
+       print("Come on in!")
+else:
+    print("Okay " + name + ", all right.\n\n\n")   
+
